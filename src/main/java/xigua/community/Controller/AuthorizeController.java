8f9b765e0809bf8be096065a8c5b51db042ace80
17-duncan <aa136142877@qq.com>
2020-03-26 01:25:31 +0800

    extract github config from code to properties

---
+++ src/main/java/xigua/community/Controller/AuthorizeController.java
@@ -0,0 +1,41 @@
+package xigua.community.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import xigua.community.dto.AccessTokenDTO;
+import xigua.community.dto.GithubUser;
+import xigua.community.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.Redirect.uri}")
+    private String RedirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String  state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(RedirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
+        return"index";
+    }
+}
--- src/main/java/xigua/community/Controller/IndexController.java
+++ src/main/java/xigua/community/Controller/IndexController.java
@@ -10,5 +10,6 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+
     }
 }
